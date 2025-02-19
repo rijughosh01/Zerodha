a5b8e05f175d458a6ccf41da86d37ed25c4f85e1
Riju Ghosh <pg4009355@gmail.com>
2025-02-19 12:21:02 +0530

    Top Bar

--- dashboard/src/components/TopBar.js
+++ dashboard/src/components/TopBar.js
@@ -1,6 +1,6 @@
 import React from "react";
-
 import Menu from "./Menu";
+import "./BuyActionWindow.css";
 
 const TopBar = () => {
   return (
@@ -8,16 +8,30 @@ const TopBar = () => {
       <div className="indices-container">
         <div className="nifty">
           <p className="index">NIFTY 50</p>
-          <p className="index-points">{100.2} </p>
-          <p className="percent"> </p>
-        </div>
-        <div className="sensex">
-          <p className="index">SENSEX</p>
           <p className="index-points">{100.2}</p>
           <p className="percent"></p>
         </div>
+        <div className="nifty-it">
+          <p className="index">NIFTY IT</p>
+          <p className="index-points">{200.4}</p>
+          <p className="percent"></p>
+        </div>
+        <div className="sensex">
+          <p className="index">SENSEX</p>
+          <p className="index-points">{300.6}</p>
+          <p className="percent"></p>
+        </div>
+        <div className="sensex-auto">
+          <p className="index">SENSEX AUTO</p>
+          <p className="index-points">{350.8}</p>
+          <p className="percent"></p>
+        </div>
+        <div className="sensex-energy">
+          <p className="index">SENSEX ENERGY</p>
+          <p className="index-points">{400.9}</p>
+          <p className="percent"></p>
+        </div>
       </div>
-
       <Menu />
     </div>
   );
